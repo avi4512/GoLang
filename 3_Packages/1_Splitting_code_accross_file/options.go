e7eb265755b086a0f21d4f09b7cac7f8f6a782c7
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-08 10:52:53 +0530

    Add files via upload

---
+++ 3_Packages/1_Splitting_code_accross_file/options.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func chooseOption() {
+	fmt.Println("What do you want to do ?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. WithDraw Amount")
+	fmt.Println("4. Exit")
+}
