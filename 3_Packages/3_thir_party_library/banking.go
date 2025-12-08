ddc2ea6b742f574dc279643c1f669ea17316972a
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-08 12:53:38 +0530

    Add files via upload

--- 3_Packages/3_thir_party_library/banking.go
+++ 3_Packages/3_thir_party_library/banking.go
@@ -1,1 +1,65 @@
+package main
 
+import (
+	"GoLang/fileOprs"
+	"fmt"
+
+	"github.com/Pallinder/go-randomdata"
+)
+
+const globalAmountFile = "balance.txt"
+
+func main() {
+
+	currentBalance, err := fileOprs.ReadFloatFromFile(globalAmountFile)
+
+	if err != nil {
+		fmt.Println("Something went Worng..!")
+		fmt.Println(err)
+		//panic("Program got Terminated..!!!!")
+	}
+	fmt.Println("Welcome the Go Banking..")
+	fmt.Println("Contact 24/7: ", randomdata.PhoneNumber())
+
+	for {
+
+		chooseOption()
+
+		var choice int
+		fmt.Print("Enter your Choice: ")
+		fmt.Scan(&choice)
+		switch choice {
+
+		case 1:
+			fmt.Printf("Current Bank Balance : %.2f\n", currentBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Enter amount to Deposit: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount > 0 {
+				currentBalance += depositAmount
+				fmt.Printf("After Deposit Amount Added: %.2f\n", currentBalance)
+				fileOprs.WriteFloatToFile(globalAmountFile, currentBalance)
+			} else {
+				fmt.Println("Deposit Amount must be GREATER than 0")
+				continue
+			}
+		case 3:
+			var withdrawAmount float64
+			fmt.Print("Enter amount to Deposit: ")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount > 0 && withdrawAmount <= currentBalance {
+				currentBalance -= withdrawAmount
+				fmt.Printf("After withdraw: %.2f\n", currentBalance)
+				fileOprs.WriteFloatToFile(globalAmountFile, currentBalance)
+			} else {
+				fmt.Println("INVALID withdraw Amount...!")
+				continue
+			}
+		default:
+			fmt.Println("Thank You..")
+			fmt.Println("For Choosing Go Bank...")
+			return
+		}
+	}
+}
