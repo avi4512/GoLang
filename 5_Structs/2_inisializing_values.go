6399aba5749b3382deb1e92401bdb031c94420ed
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-09 14:14:56 +0530

    Create 2_inisializing_values.go

---
+++ 5_Structs/2_inisializing_values.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Defining Struct
+
+type userData struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdOn time.Time
+}
+
+func main() {
+
+	userFirstName := getData("Enter your First Name: ")
+	userLastName := getData("Enter your Last Name: ")
+	userBirthDate := getData("Enter your Data of Birth DD/MM/YYYY: ")
+
+	var appUser userData
+
+	appUser = userData{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthDate,
+		createdOn: time.Now(),
+	}
+
+	outputDetails(appUser)
+
+}
+
+func outputDetails(u userData) {
+
+	println(u.firstName, u.lastName, u.birthDate)
+}
+
+func getData(msgText string) string {
+	fmt.Print(msgText)
+	var data string
+	fmt.Scan(&data)
+	return data
+}
