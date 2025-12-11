662adba7e950a4f3e3a566c82c35d61a18c9fedf
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-11 16:33:37 +0530

    Add files via upload

---
+++ 6_interfaces/3_embedded_interface/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"booksProject/note"
+	"booksProject/todo"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type saver interface {
+	SaveTOjson() error
+}
+
+// type outputer interface {
+// 	Output()
+// }
+
+type outputable interface {
+	saver
+	Output()
+}
+
+// type outputable interface {
+// 	SaveTOjson() error
+// 	Output()
+// }
+
+func main() {
+
+	title, content := getNoteData()
+	text := getData("Todo Text:")
+
+	userTodo, err := todo.New(text)
+
+	if err != nil {
+		fmt.Print(err, " Something Wrong!!!")
+		return
+	}
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Print(err, " Something Wrong!!!")
+		return
+	}
+
+	err = outputData(userNote)
+
+	if err != nil {
+		fmt.Println("Error in saveData..")
+		fmt.Println(err)
+	}
+
+	err = outputData(userTodo)
+
+	if err != nil {
+		fmt.Println("Error in saveData..")
+		fmt.Println(err)
+	}
+
+}
+
+func outputData(data outputable) error {
+	data.Output()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+
+	err := data.SaveTOjson()
+
+	if err != nil {
+		fmt.Println("File Not Saved:")
+		return err
+	}
+
+	note_path, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("File Not Saved")
+		return err
+	}
+
+	fmt.Printf("File Saved Successfully..at %s\n", note_path)
+	return nil
+
+}
+
+func getNoteData() (string, string) {
+
+	title := getData("Enter the tile:")
+	Content := getData("Enter the Content: ")
+	return title, Content
+
+}
+func getData(text string) string {
+	fmt.Print(text)
+	read := bufio.NewReader(os.Stdin)
+	text, err := read.ReadString('\n')
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return text
+}
