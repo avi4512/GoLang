0ae9720d1a15420604e5d300d7a440e860523403
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-12 12:10:41 +0530

    Create 2_slicing.go

---
+++ 7_Collections/2_slicing.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//creation of array
+
+	prices := [4]float64{12.34, 15.43, 16.78, 23.56}
+	slicePrice := prices[1:3]
+	array_f := prices[:3]
+	array_l := prices[2:]
+
+	fmt.Println("Original Array:", prices)
+	fmt.Println("Sliced Array:", slicePrice)
+	fmt.Println("Slice from beging to given index:", array_f)
+	fmt.Println("Slice from given index to last:", array_l)
+}
