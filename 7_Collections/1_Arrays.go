e88c25a5efcdee937148a80a25163e1152d44f20
avi4512 <avireddy4512@gmail.com>
2025-12-12 11:55:12 +0530

    Creation of Array

--- 7_Collections/1_Arrays.go
+++ 7_Collections/1_Arrays.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	//creation of array
+	prices := [4]float64{12.34, 15.43, 16.78}
+
+	fmt.Println("Length of the Array:", len(prices))
+
+	fmt.Println(prices)
+}
