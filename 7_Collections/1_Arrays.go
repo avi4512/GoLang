1ea240220793d781a09c426a877b323ac8165d18
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-12-12 12:00:54 +0530

    Update 1_Arrays.go

--- 7_Collections/1_Arrays.go
+++ 7_Collections/1_Arrays.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 
 	//creation of array
-	prices := [4]float64{12.34, 15.43, 16.78}
 
-	fmt.Println("Length of the Array:", len(prices))
+	prices := [4]float64{12.34, 15.43, 16.78, 23.56}
+	var discount [3]float64 = [3]float64{2.34, 13.67, 17.89}
 
 	fmt.Println(prices)
+	fmt.Println(discount)
 }
