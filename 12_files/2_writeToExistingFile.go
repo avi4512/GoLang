1d9f4601b7913e0b2e2f0e558a58a5eeec09e641
avi4512 <144973797+avi4512@users.noreply.github.com>
2026-01-06 08:24:19 +0530

    Update 2_writeToExistingFile.go

--- 12_files/2_writeToExistingFile.go
+++ 12_files/2_writeToExistingFile.go
@@ -5,17 +5,6 @@ import (
 	"os"
 )
 
-func writeToFile(text string) {
-	fmt.Print(text)
-	var amount float64
-	fmt.Scan(&amount)
-
-	str_amount := fmt.Sprint(amount)
-	os.WriteFile("amount_file.txt", []byte(str_amount), 0644)
-	path, _ := os.Getwd()
-	fmt.Println("Success.....")
-	fmt.Println("File saved at", path)
-}
 
 func writeToExistingFile(text string) {
 
